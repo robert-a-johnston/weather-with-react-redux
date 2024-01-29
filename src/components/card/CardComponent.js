b9844ef992239fc7f6af1de262ef325f258c31f0
Johnston, Robert <robert.a.johnston@accenture.com>
2024-01-29 12:55:50 -0500

    Initial commit set up tailwind and card

---
+++ src/components/card/CardComponent.js
@@ -0,0 +1,22 @@
+// Create a react card component
+
+import React from "react";
+
+function Card(props) {
+    return (
+        <div className="card">
+        <div className="card-content">
+          <div className="card-title">New York</div>
+          <p className="card-text">
+            Lorem ipsum dolor sit amet, consectetur adipisicing elit. Voluptatibus quia, nulla! Maiores et perferendis eaque, exercitationem praesentium nihil.
+          </p>
+        </div>
+        <div className="card-content">
+          <span className="btn btn-details">Details</span>
+          <span className="btn btn-remove">Remove</span>
+        </div>
+      </div>
+  );
+}
+
+export default Card;
